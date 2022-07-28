3a3d852c11d779f459db7b62db53ce0f97e3ccbb
Jack200133 <angelitocarrera@gmail.com>
2022-07-28 17:35:46 -0600

    Clase 26 de Julio

--- gl.py
+++ gl.py
@@ -1,5 +1,7 @@
 import struct
+import numpy as np
 from collections import namedtuple
+from obj import Obj
 
 V2 = namedtuple('Vertex2', ['x', 'y'])
 
@@ -135,6 +137,139 @@ class Renderer(object):
                 if self.boundaries(i, j, poly):
                     self.glPoint(i, j, clr)
 
+    def glFillTriangle(self, v0,v1,v2, clr=None):
+        #Scan Algorithm
+
+        if v0[1] < v1[1]:
+            v0, v1 = v1, v0
+        if v0[1] < v2[1]:
+            v0, v2 = v2, v0
+        if v1[1] < v2[1]:
+            v1, v2 = v2, v1
+
+        def flattBottom(vA,vB,vC):
+            try:
+                slopeAB = (vB[0]-vA[0])/(vB[1]-vA[1])
+                slopeAC = (vC[0]-vA[0])/(vC[1]-vA[1])
+            except ZeroDivisionError:
+                pass
+            else:
+            #slopeAB = (vB[1]-vA[1])/(vB[0]-vA[0])
+            #slopeAC = (vC[1]-vA[1])/(vC[0]-vA[0])
+
+                x0 = v1[0]
+                x1 = v2[1]
+
+                for y in range(v1[1],v0[1]+1):
+                    self.glLine(int(x0), y, int(x1), y, clr)
+                    x0 += slopeAB
+                    x1 += slopeAC
+                    if x0 > x1:
+                        x0, x1 = x1, x0
+
+        def flattTop(vA,vB,vC):
+            try:
+                slopeCA = (vC[0]-vA[0])/(vC[1]-vA[1])
+                slopeCB = (vC[0]-vB[0])/(vC[1]-vB[1])
+            except ZeroDivisionError:
+                pass
+            else:
+                x0 = vA[0]
+                x1 = vB[0]
+
+                for y in range(vA[1],vC[1],-1):
+                    self.glLine(int(x0), y, int(x1), y, clr)
+                    x0 -= slopeCA
+                    x1 -= slopeCB
+                    if x0 > x1:
+                        x0, x1 = x1, x0
+
+        if v1[1] == v2[1]:
+            #Parte plana abajo
+            flattBottom(v0,v1,v2)
+            
+        elif v0[1] == v1[1]:
+            #Parte plana arriba
+
+            flattTop(v0,v1,v2)
+        else:
+            # Se parte 
+            #Teorema de intercepto
+
+            vD = [v0[0] + ((v1[1]-v0[1])/(v2[1]-v0[1])) * (v2[0]-v0[0]),v1[1]]
+
+            flattBottom(v0,v1,vD)
+            flattTop(v1,vD,v2)
+            pass
+
+        self.glLine(v0[0],v0[1],v1[0],v1[1],clr)
+        self.glLine(v1[0],v1[1],v2[0],v2[1],clr)
+        self.glLine(v2[0],v2[1],v0[0],v0[1],clr)
+
+    def glLoadModel2(self,filename):
+        model = Obj(filename)
+
+        for face in model.faces:
+            vcount = len(face)
+            for j in range(vcount):
+                f1 = face[j][0]
+                f2 = face[(j+1) % vcount][0]
+                x1 = model.vertices[f1 - 1][0]
+                y1 = model.vertices[f1 - 1][1]
+                x2 = model.vertices[f2 - 1][0]
+                y2 = model.vertices[f2 - 1][1]
+                self.glLine(x1, y1, x2, y2)
+
+
+    def glLoadModel(self,filename,translate,rotate,scale):
+        model = Obj(filename)
+        modelMatrix = self.glCreateObjectMatrix(translate, rotate, scale)
+
+        for face in model.faces:
+            vertCount = len(face)
+            for vert in range(vertCount):
+                v0 = model.vertices[ face[vert][0] - 1]
+                v1 = model.vertices[ face[(vert + 1) % vertCount][0] - 1]
+
+                v0 = self.glTransform(v0, modelMatrix)
+                v1 = self.glTransform(v1, modelMatrix)
+
+                self.glLine(v0[0], v0[1], v1[0], v1[1])
+
+    def glCreateObjectMatrix2(self, translate, rotate, scale):
+        modelMatrix = np.identity(4)
+        modelMatrix = self.glTranslate(modelMatrix, translate)
+        modelMatrix = self.glRotate(modelMatrix, rotate)
+        modelMatrix = self.glScale(modelMatrix, scale)
+        return modelMatrix
+
+    def glTransform(self, vertex, matrix):
+
+        v = V4(vertex[0], vertex[1], vertex[2], 1)
+        vt = matrix @ v
+        vt = vt.tolist()[0]
+        vf = V3(vt[0] / vt[3],
+                vt[1] / vt[3],
+                vt[2] / vt[3])
+
+        return vf
+
+    def glCreateObjectMatrix(self, translate, rotate , scale):
+
+            translation = np.matrix([[1, 0, 0, translate.x],
+                                    [0, 1, 0, translate.y],
+                                    [0, 0, 1, translate.z],
+                                    [0, 0, 0, 1]])
+
+            rotation = np.identity(4)
+
+            scaleMat = np.matrix([[scale.x, 0, 0, 0],
+                                [0, scale.y, 0, 0],
+                                [0, 0, scale.z, 0],
+                                [0, 0, 0, 1]])
+
+            return translation * rotation * scaleMat
+
     def glFinish(self, filenames):
 
         with open(filenames, "wb") as file:
@@ -162,3 +297,4 @@ class Renderer(object):
             for y in range(self.height):
                 for x in range(self.width):
                     file.write(self.pixels[x][y])
+        
