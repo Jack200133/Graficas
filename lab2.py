81390fc56a8fd4c5c1fbeac6c04e874ac70b79e9
Jack200133 <angelitocarrera@gmail.com>
2022-08-16 18:58:19 -0600

    Void shader

--- lab2.py
+++ lab2.py
@@ -1,5 +1,5 @@
 from gl import Renderer, color
-from shaders import flat,gourad,toon,glow
+from shaders import flat,gourad,toon,glow,void
 from texture import Texture
 
 render = Renderer()
@@ -19,7 +19,7 @@ render.glClear()
 
 modelP = [0,0,-10]
 
-# render.active_texture = Texture("./models/model.bmp")
+#render.active_texture = Texture("./models/model.bmp")
 # render.glLoadModel("./models/model.obj",
 #                    translate=modelP,
 #                    rotate=[0, 0, 0],
@@ -27,20 +27,20 @@ modelP = [0,0,-10]
 
 #render.glLookAt(modelP, [-5,-2,0])
 
-render.active_shader = gourad
+#render.active_shader = gourad
 #render.glLookAt([-3, -4,-10], [3,-4,1])
 
-render.active_texture = Texture("./rocket/rocket.bmp")
-render.glLookAt([0, 0,-10], [0,0,-3])
-render.glLoadModel("./rocket/rocket.obj",
-                   translate=[-3, -3,-10],
-                   scale=[0.02, 0.02,0.02])
-render.active_shader = toon
-render.glLoadModel("./rocket/rocket.obj",
-                   translate=[3, -3,-10],
-                   scale=[0.02, 0.02,0.02])
+#render.active_texture = Texture("./rocket/rocket.bmp")
+# render.glLookAt([0, 0,-10], [0,0,-3])
+# render.glLoadModel("./rocket/rocket.obj",
+#                    translate=[-3, -3,-10],
+#                    scale=[0.02, 0.02,0.02])
+# render.active_shader = toon
+# render.glLoadModel("./rocket/rocket.obj",
+#                    translate=[3, -3,-10],
+#                    scale=[0.02, 0.02,0.02])
 
-render.active_shader = glow
+render.active_shader = void
 render.glLoadModel("./rocket/rocket.obj",
                    translate=[0, -3,-10],
                    scale=[0.02, 0.02,0.02])
--- shaders.py
+++ shaders.py
@@ -174,3 +174,60 @@ def glow(render, **kwargs):
         return r, g, b
     else:
         return 0,0,0
+
+def void(render, **kwargs):
+
+    u, v, w = kwargs["baryCoords"]
+    b, g, r = kwargs["vcolor"]
+    tA, tB, tC = kwargs["texCoords"]
+    nA, nB, nC = kwargs["normals"]
+
+    b /= 255
+    g /= 255
+    r /= 255
+
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = [nA[0] * u + nB[0] * v + nC[0] * w,
+                               nA[1] * u + nB[1] * v + nC[1] * w,
+                               nA[2] * u + nB[2] * v + nC[2] * w]
+
+    intensidad = producto_punto(triangleNormal, inversa(render.dirLight))
+
+    b *= intensidad
+    g *= intensidad
+    r *= intensidad
+
+    camFoward = render.camMatrix[0][2], render.camMatrix[1][2], render.camMatrix[2][2]
+    glowAmount = 1 - producto_punto(triangleNormal, camFoward)
+    antiglowAmount = producto_punto(triangleNormal, camFoward)
+
+    color = [1,0.5,0]
+    if glowAmount < antiglowAmount:
+        glowAmount = antiglowAmount
+        color = [0,0,0]
+
+    #if glowAmount <= 0: glowAmount = 0
+
+
+    b += color[2] * glowAmount
+    g += color[1] * glowAmount
+    r += color[0] * glowAmount
+
+    if b > 1: b = 1
+    if g > 1: g = 1
+    if r > 1: r = 1
+
+    if intensidad > 0:
+        return r, g, b
+    else:
+        return 0,0,0
