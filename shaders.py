d3ecd5f573c4527fe7e6e440d3ec36bdcd10920e
Jack200133 <angelitocarrera@gmail.com>
2022-08-18 19:40:25 -0600

    shaders

--- shaders.py
+++ shaders.py
@@ -213,8 +213,8 @@ def void(render, **kwargs):
 
     color = [1,0.5,0]
     if glowAmount < antiglowAmount:
-        glowAmount = antiglowAmount
         color = [0,0,0]
+        r,g,b= 0,0,0
 
     #if glowAmount <= 0: glowAmount = 0
 
@@ -226,6 +226,85 @@ def void(render, **kwargs):
     if b > 1: b = 1
     if g > 1: g = 1
     if r > 1: r = 1
+
+    if intensidad > 0:
+        return r, g, b
+    else:
+        return 0,0,0
+
+def inverso(render, **kwargs):
+
+    u, v, w = kwargs["baryCoords"]
+    b, g, r = kwargs["vcolor"]
+    tA, tB, tC = kwargs["texCoords"]
+    nA, nB, nC = kwargs["normals"]
+
+    b /= 255
+    g /= 255
+    r /= 255
+
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = [nA[0] * u + nB[0] * v + nC[0] * w,
+                               nA[1] * u + nB[1] * v + nC[1] * w,
+                               nA[2] * u + nB[2] * v + nC[2] * w]
+
+    intensidad = producto_punto(triangleNormal, inversa(render.dirLight))
+
+    b *= intensidad
+    g *= intensidad
+    r *= intensidad
+
+    b = 1 - b
+    g = 1 - g
+    r = 1 - r
+
+    if intensidad > 0:
+        return r, g, b
+    else:
+        return 0,0,0
+
+
+def algo(render, **kwargs):
+
+    u, v, w = kwargs["baryCoords"]
+    b, g, r = kwargs["vcolor"]
+    tA, tB, tC = kwargs["texCoords"]
+    nA, nB, nC = kwargs["normals"]
+
+    b /= 255
+    g /= 255
+    r /= 255
+
+    if render.active_texture:
+        # P = Au + Bv + Cw
+        tU = tA[0] * u + tB[0] * v + tC[0] * w
+        tV = tA[1] * u + tB[1] * v + tC[1] * w
+
+        texColor = render.active_texture.getColor(tU, tV)
+
+        b *= texColor[2]
+        g *= texColor[1]
+        r *= texColor[0]
+
+    triangleNormal = [nA[0] * u + nB[0] * v + nC[0] * w,
+                               nA[1] * u + nB[1] * v + nC[1] * w,
+                               nA[2] * u + nB[2] * v + nC[2] * w]
+
+    intensidad = producto_punto(triangleNormal, inversa(render.dirLight))
+
+    b *= (abs(nA[0])*abs(nB[0])*abs(nC[0]))*intensidad %1
+    g *= (abs(nA[1])*abs(nB[1])*abs(nC[1]))*intensidad %1
+    r *= (abs(nA[2])*abs(nB[2])*abs(nC[2]))*intensidad %1
 
     if intensidad > 0:
         return r, g, b
